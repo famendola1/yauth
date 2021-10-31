106f9d53eaea65280de35b3699b31b8959f238ab
Fabio Amendola <fabio.amendola20@gmail.com>
2021-10-31 12:24:49 -0400

    Add JSON functionality

--- yauth.go
+++ yauth.go
@@ -4,6 +4,9 @@ import (
   "bufio"
   "bytes"
   "context"
+  "encoding/json"
+  "io/ioutil"
+  "os"
 
   "github.com/toqueteos/webbrowser"
   "golang.org/x/oauth2"
@@ -14,7 +17,25 @@ var (
   redirectURL string = "oob"
 )
 
-func CreateTokenFromCredentials(clientID string, clientSecret string) (*oauth2.Token, error) {
+type YAuth struct {
+  Config *oauth2.Config `json:"config"`
+  Token *oauth2.Token `json:"token"`
+}
+
+func (y YAuth) WriteToFile(filePath string) error {
+  file, err := json.MarshalIndent(y, "", "")
+  if err != nil {
+    return err
+  }
+
+  if err := ioutil.WriteFile(filePath, file, 0644); err != nil {
+    return err
+  }
+
+  return nil
+}
+
+func CreateYAuthFromCredentials(clientID string, clientSecret string) (*YAuth, error) {
   config := oauth2.Config{
     ClientID: clientID,
     ClientSecret: clientSecret,
@@ -36,5 +57,26 @@ func CreateTokenFromCredentials(clientID string, clientSecret string) (*oauth2.T
     return nil, err
   }
 
-  return token, nil
+  auth := YAuth{Config: config, Token: token}
+  return &auth, nil
+}
+
+func CreateYAuthFromJson(filePath string) (*YAuth, error) {
+  jsonFile, err := os.Open(filePath)
+  if err != nil {
+    return nil, err
+  }
+  defer jsonFile.Close()
+
+  byteValue, err := ioutil.ReadAll(jsonFile)
+  if err != nil {
+    return nil, err
+  }
+
+  auth := new(YAuth)
+  if err := json.Unmarshal(byteValue, auth); err != nil {
+    return nil, err
+  }
+
+  return auth, nil
 }
